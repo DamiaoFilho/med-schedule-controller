5592cfce051398dc3e410924409bd1ff6f2eb210
DamiaoFilho <damiaofilho0123@gmail.com>
2024-04-11 14:51:49 -0300

    working

--- src/appointments/routes.js
+++ src/appointments/routes.js
@@ -14,4 +14,46 @@ router.get("/appointments", function (req, res) {
   });
 });
 
+router.post("/appointments/create", function (req, res) {
+  appointments.service.Create(req.body, function (err, response) {
+    if (err) {
+      console.error("Error creating appointment:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log("Appointment created successfully:", response);
+      res.status(201).json(response);
+    }
+  });
+});
+
+
+router.put("/appointments/:id", function (req, res) {
+  const appointmentId = req.params.id;
+  const updatedData = req.body;
+
+  appointments.service.Update({ id: appointmentId, ...updatedData }, function (err, response) {
+    if (err) {
+      console.error("Error updating appointment:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log("Appointment updated successfully:", response);
+      res.status(200).json(response);
+    }
+  });
+});
+
+router.delete("/appointments/:id", function (req, res) {
+  const appointmentId = req.params.id;
+
+  appointments.service.Destroy({ id: appointmentId }, function (err, response) {
+    if (err) {
+      console.error("Error deleting appointment:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      console.log("Appointment deleted successfully");
+      res.status(204).end(); 
+    }
+  });
+});
+
 module.exports = router;
